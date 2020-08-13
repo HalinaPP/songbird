d9f71d2607df5a0d59497afb5e0c2e3d56bd1364
galina <gpischalo@belta.by>
2020-08-13 16:08:25 +0300

    feat:  create content collection

--- src/helpers/transport-quiz.js
+++ src/helpers/transport-quiz.js
@@ -1,21 +1,130 @@
 const transportData = [
     {
-        publicTransopt:[
+        alias: 'publicTransport',
+        title:'Общественный',
+        elements:[
             {
                 id: 1,
                 name: 'Автобус',
                 species: 'Bus',
-                description: '',
-                image: './images/bus.jpg',
-                audio: './audio/bus.mp3'
+                description: 'Описание',
+                image: '/images/public-transport/bus.jpg',
+                audio: '/audio/public-transport/bus.mp3'
+            },
+            { 
+                id: 2,
+                name: 'Троллейбус',
+                species: 'trolleybus',
+                description: 'Описание2',
+                image: '/images/public-transport/trolleybus.jpg',
+                audio: '/audio/public-transport/trolleybus.mp3'
+
             },
             {
-
+                id: 3,
+                name: 'Метро',
+                species: 'Subway',
+                description: 'Описание',
+                image: '/images/public-transport/subway.jpg',
+                audio: '/audio/public-transport/subway.mp3'
+            },
+            {
+                id: 4,
+                name: 'Паром',
+                species: 'Parom',
+                description: 'Описание',
+                image: '/images/public-transport/parom.jpg',
+                audio: '/audio/public-transport/parom.mp3'
+            },
+            {
+                id: 5,
+                name: 'Пассажирский самолет',
+                species: 'Plane',
+                description: 'Описание',
+                image: '/images/public-transport/plane.jpg',
+                audio: '/audio/public-transport/plane.mp3'
+            },
+            {
+                id: 6,
+                name: 'Трамвай',
+                species: 'Tramvai',
+                description: 'Описание',
+                image: '/images/public-transport/tramvai.jpg',
+                audio: '/audio/public-transport/tramvai.mp3'
             }
-        ],
-        waterTrnasport:[{}],
-        railwayTransport:[{}]
-    }  
+        ]
+    },
+
+    {
+        alias: 'waterTransport',
+        title:'Водный',
+        elements:[
+            {
+                id: 1,
+                name: 'Автобус',
+                species: 'Bus',
+                description: 'Описание',
+                image: './images/bus.jpg',
+                audio: './audio/bus.mp3'
+            }
+        ]
+    },
+    {
+        alias: 'railwayTransport',
+        title:'Железнодорожный',
+        elements:[
+            {
+                id: 1,
+                name: 'Автобус',
+                species: 'Bus',
+                description: 'Описание',
+                image: './images/bus.jpg',
+                audio: './audio/bus.mp3'
+            }
+        ]
+    },
+    {
+        alias: 'airTransport',
+        title:'Воздушный',
+        elements:[
+            {
+                id: 1,
+                name: 'Автобус',
+                species: 'Bus',
+                description: 'Описание',
+                image: './images/bus.jpg',
+                audio: './audio/bus.mp3'
+            }
+        ]
+    },
+    {
+        alias: 'twoWheelerTransport',
+        title: 'Двухколесный',
+        elements:[
+            {
+                id: 1,
+                name: 'Автобус',
+                species: 'Bus',
+                description: 'Описание',
+                image: './images/bus.jpg',
+                audio: './audio/bus.mp3'
+            }
+        ]
+    },
+    {
+        alias: 'variousTransport',
+        title:'Четырехколесный',
+        elements:[
+            {
+                id: 1,
+                name: 'Автобус',
+                species: 'Bus',
+                description: 'Описание',
+                image: './images/bus.jpg',
+                audio: './audio/bus.mp3'
+            }
+        ]
+    }
 ];
-  
-  export default transportData;
+
+export default transportData;
