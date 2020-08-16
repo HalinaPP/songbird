da752877f54616c2099e8433d97163036a6d9935
galina <gpischalo@belta.by>
2020-08-17 01:51:56 +0300

    refactor: clean code

--- src/components/score/score.js
+++ src/components/score/score.js
@@ -2,14 +2,9 @@ import React, {Component} from 'react';
 import './score.css';
 
 class Score  extends Component{
-    /*state = {
-        score:0
-    }*/
-
     render(){
-        //const currentScore = this.state.score + this.props.scoreOfLevel;
         return (
-        <span className="score">Score: {this.props.score}</span>
+             <span className="score">Score: {this.props.score}</span>
         );
     }
 }
