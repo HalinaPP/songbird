f2390dbe687ca33a105bd28c160b76af433d0a1b
galina <gpischalo@belta.by>
2020-08-20 00:35:46 +0300

    feat: add audio for congratulaion component

--- src/components/congratulations/congratulations.js
+++ src/components/congratulations/congratulations.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import BtnNextlevel from '../btn-next-level';
 import logo from '../../helpers/logo.png';
+import congratulationsAudio from '../../helpers/congratulations.mp3';
+import winnerAudio from '../../helpers/winner.mp3';
 import './congratulations.css';
 
 const Congratulations = (props) =>{
-    //const  {score, maxCount} = props;
+    const  {score, maxCount} = props;
+    const winner = score === maxCount ? true : false;
+    const audioFile = winner ? winnerAudio : congratulationsAudio;
+    const audio = new Audio(audioFile);
+    audio.play();
     return (
         <div className = "congratulations">
             <img src={logo} alt='Transport Quiz' title='Transport Quiz' />
             <div className="col-12 card">
                 <h1>Поздравляем!</h1>
-                Вы набрали <span>{props.score}</span> из <span>{props.maxCount}</span> возможных баллов!
+                {   winner 
+                    ? <div>Вы набрали максимально возможное количество баллов - <span>{maxCount}</span>!</div>
+                    : <div>Вы набрали <span>{score}</span> из <span>{maxCount}</span> возможных баллов!</div>
+                }
             </div>
             <BtnNextlevel 
                 active={true}  
